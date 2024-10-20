433aa369c7f067d93c63df65d190e5352f70ca95
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2024-10-21 00:40:56 +0530

    more updates

--- cursor/cursor.js
+++ cursor/cursor.js
@@ -6,8 +6,9 @@ hero.addEventListener('mousemove', function(dets){
     gsap.to(cursor,{
         x:dets.x-8,
         y:dets.y-8,
-        duration: 1,
-        ease:"back.out",
+        duration: 0.5,
+        // ease:"back.out",
+        ease: "power4.out",
         // ease:'elastic.out(1,0.3)',
 
 
